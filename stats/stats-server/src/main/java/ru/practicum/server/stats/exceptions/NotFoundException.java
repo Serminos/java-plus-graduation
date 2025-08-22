aac085e5c31f52b24c0f26e7c5c35bbca556cecc
KuznetsovOS <KuznetsovOS@kadastr.ru>
2025-08-22 14:23:13 +0300

    feat: add collector and delete stats-sercer and stats-dto

--- stats/stats-server/src/main/java/ru/practicum/server/stats/exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.server.stats.exceptions;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
-
